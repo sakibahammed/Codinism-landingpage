57f2ca4850be798018153ed063191e9b328ae5fd
shariarhossain17 <shariarhossain23@gmail.com>
2026-03-30 14:54:56 +0600

    solved

--- src/components/ui/sections/Stats.tsx
+++ src/components/ui/sections/Stats.tsx
@@ -41,7 +41,7 @@ const Stats = () => {
         {/* Main Content */}
         <div className="relative z-[1] w-full max-w-5xl px-8">
           <div className="grid grid-cols-3 items-center">
-            <div className="flex items-center justify-center">
+            <div className="flex flex-col items-center">
               <div className="relative w-[220px] h-[90px]">
                 <a href="https://www.edulavo.com" target="_blank">
                   <Image
@@ -52,6 +52,13 @@ const Stats = () => {
                   />
                 </a>
               </div>
+
+              <p className="text-gray-200 text-[14px] mt-6  leading-relaxed pr-8">
+                Edulavo is a personal AI-powered skill coach designed to understand how people learn
+                and adapt to their goals. It creates a smart, personalized learning path tailored to
+                individual pace and ambitions—helping users grow faster and more effectively. Join
+                25,000+ daily learners already leveling up with Edulavo.
+              </p>
             </div>
 
             <div className="flex flex-col items-center">
@@ -66,14 +73,15 @@ const Stats = () => {
                 </a>
               </div>
 
-              <p className="text-gray-200 text-[14px] mt-6 text-center leading-relaxed ">
-                Makes Their Dream come
-                <br />
-                in reality.
+              <p className="text-gray-200 text-[14px] mt-6 leading-relaxed ">
+                Phitron helps build a strong foundation and secure a career with a super supportive
+                learning platform. Master competitive programming, problem solving, logic building,
+                and core programming through structured, hands-on learning. Join 10,000+ daily
+                learners advancing toward a strong tech future with Phitron.
               </p>
             </div>
 
-            <div className="flex items-center justify-center">
+            <div className="flex flex-col items-center">
               <div className="relative w-[220px] h-[90px]">
                 <a href="https://www.programming-hero.com/" target="_blank">
                   <Image
@@ -84,6 +92,14 @@ const Stats = () => {
                   />
                 </a>
               </div>
+
+              <p className="text-gray-200 text-[14px] mt-6  leading-relaxed pl-8">
+                Programming Hero is on a mission to deliver personalized learning and empower
+                individuals to kick-start their careers. With engaging, interactive content and a
+                learner-focused approach, it helps users build real-world skills effectively.
+                Trusted by 40,000+ daily learners, Programming Hero is shaping the next generation
+                of developers.
+              </p>
             </div>
           </div>
         </div>
